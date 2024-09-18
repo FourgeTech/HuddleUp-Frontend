b4e20cf7f1956de7c4ebdc2043812326f3f78ce1
Daniel Marais <ST10132589@vcconnect.edu.za>
2024-09-18 13:37:26 +0200

    Added fragment_announcements_page and the item for it. Also added the AnnouncementModel and AnnouncementAdapdter

---
+++ app/src/main/java/tech/fourge/huddleup_frontend/Utils/AnnouncementAdapter.kt
@@ -0,0 +1,39 @@
+package tech.fourge.huddleup_frontend.Utils
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import tech.fourge.huddleup_frontend.R //
+
+class AnnouncementAdapter(private val announcements: List<AnnouncementModel>) :
+    RecyclerView.Adapter<AnnouncementAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_announcement, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val announcement: AnnouncementModel = announcements[position]
+        holder.tvName.text = announcement.name
+        holder.tvTime.text = announcement.time
+        holder.tvAnnouncementText.text = announcement.announcementText
+        holder.tvViews.text = announcement.views
+    }
+
+    override fun getItemCount(): Int {
+        return announcements.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvName: TextView = itemView.findViewById(R.id.tvName)
+        var tvTime: TextView = itemView.findViewById(R.id.tvTime)
+        var tvAnnouncementText: TextView = itemView.findViewById(R.id.tvAnnouncementText)
+        var tvViews: TextView = itemView.findViewById(R.id.tvViews)
+        var editReply: EditText = itemView.findViewById(R.id.editReply)
+    }
+}
---
+++ app/src/main/java/tech/fourge/huddleup_frontend/Utils/AnnouncementModel.kt
@@ -0,0 +1,8 @@
+package tech.fourge.huddleup_frontend.Utils
+
+data class AnnouncementModel(
+    val name: String,
+    val time: String,
+    val announcementText: String,
+    val views: String
+)
